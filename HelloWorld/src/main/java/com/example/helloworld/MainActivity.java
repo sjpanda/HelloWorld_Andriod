ea1b585e7ab0e6c792a63151e0c369fac04dd62c
Jing <js.shujing@msn.com>
2014-01-30 12:03:17 +0100

    try wcf + soap

--- HelloWorld/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/src/main/java/com/example/helloworld/MainActivity.java
@@ -12,14 +12,40 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
     public final static String EXTRA_MESSAGE = "com.example.helloworld.MESSAGE";
 
+
+    private static final String METHOD_NAME = "Connecter";
+
+    private static final String NAMESPACE = "http://tempuri.org/";
+
+    private static final String URL = "http://localhost:5645/ServiceAuthentification.svc";
+
+    final String SOAP_ACTION = "http://tempuri.org/IServiceAuthentification/Connecter";
+
+    TextView tv;
+    StringBuilder sb;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+        tv = new TextView(this);
+        sb = new StringBuilder();
+        call();
+        tv.setText(sb.toString());
+        setContentView(tv);
+
+
+
+
+        /*setContentView(R.layout.activity_main);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
@@ -27,8 +53,38 @@ public class MainActivity extends ActionBarActivity {
                     .commit();
         }
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);*/
     }
+
+    public void call() {
+        try {
+
+            SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
+
+            request.addProperty("Name", "Qing");
+
+            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
+                    SoapEnvelope.VER11);
+            envelope.dotNet = true;
+            envelope.setOutputSoapObject(request);
+
+
+            HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
+            androidHttpTransport.call(SOAP_ACTION, envelope);
+            SoapPrimitive result = (SoapPrimitive)envelope.getResponse();
+
+            //to get the data
+            String resultData = result.toString();
+            // 0 is the first object of data
+
+
+            sb.append(resultData + "\n");
+        } catch (Exception e) {
+            sb.append("Error:\n" + e.getMessage() + "\n");
+        }
+
+    }
+}
 
 
     @Override
